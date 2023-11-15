1c6dbce97625a3be9d17d759c86a0cafc1aaa25d
sheldon <liu_2648349896@126.com>
2023-11-16 01:49:28 +0800

    二分法

---
+++ src/main/java/com/sheldon/util/ArrayUtils.java
@@ -0,0 +1,19 @@
+package com.sheldon.util;
+
+/**
+ * @ClassName ArrayUtils
+ * @Author 26483
+ * @Date 2023/11/16 0:51
+ * @Version 1.0
+ * @Description TODO
+ */
+public class ArrayUtils {
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+}
